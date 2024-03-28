1b96848b4b1a3e5baebf57fdc33cbf02d6edf5b3
Rohit Sayeeganesh <rsbby@mst.edu>
2024-03-28 12:16:45 -0500

    Another commit implementing BookingConsultation.js

--- med_appt/src/App.js
+++ med_appt/src/App.js
@@ -4,7 +4,10 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/LandingPage';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
-
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import FindDoctorSearchIC from './Components/InstantConsultation/FindDoctorSearchIC/FindDoctorSearchIC';
+import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
+import BookingConsultation from './Components/BookingConsultation';
 function App() {
   return (
     <div className="App">
@@ -15,7 +18,9 @@ function App() {
               <Route exact path="/" Component={Landing_Page} />
               <Route path="/signup" Component={Sign_Up} />
               <Route path="/login" Component={Login} />
-
+              <Route path="/instant-consultation" Component={ InstantConsultation } />
+              <Route path="/search/doctors" Component={FindDoctorSearch} />
+              <BookingConsultation/>
             </Routes>
          </switch>
         </BrowserRouter>
