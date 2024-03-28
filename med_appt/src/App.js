bedc3198e3f9736599045e87a9cc3ea1d76c159a
Rohit Sayeeganesh <rsbby@mst.edu>
2024-03-28 11:53:52 -0500

    Another commit upon another

--- med_appt/src/App.js
+++ med_appt/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/LandingPage';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
+import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
 function App() {
   return (
     <div className="App">
@@ -14,7 +15,7 @@ function App() {
               <Route exact path="/" Component={Landing_Page} />
               <Route path="/signup" Component={Sign_Up} />
               <Route path="/login" Component={Login} />
-            </Routes>
+              <Route path="/instant-consultation" element={<InstantConsultation />} />            </Routes>
          </switch>
         </BrowserRouter>
     </div>
