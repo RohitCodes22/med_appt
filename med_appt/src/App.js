8589a7c5b68b0dae3e1c50a781e61835a68d6d10
Rohit Sayeeganesh <rsbby@mst.edu>
2024-03-27 22:21:56 -0500

    What I have so far ig lol. This is another battle for another day

--- med_appt/src/App.js
+++ med_appt/src/App.js
@@ -1,18 +1,22 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
-import LandingPage from './Components/Landing_Page/LandingPage';
-
+import Landing_Page from './Components/Landing_Page/LandingPage';
+import Sign_Up from './Components/Sign_Up/Sign_Up';
+import Login from './Components/Login/Login';
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          {/* Add other routes here */}
-        </Routes>
-      </BrowserRouter>
+        <BrowserRouter>
+         <Navbar/>
+         <switch>
+            <Routes>
+              <Route exact path="/" Component={Landing_Page} />
+              <Route path="/signup" Component={Sign_Up} />
+              <Route path="/login" Component={Login} />
+            </Routes>
+         </switch>
+        </BrowserRouter>
     </div>
   );
 }
--- med_appt/src/Components/Landing_Page/LandingPage.js
+++ med_appt/src/Components/Landing_Page/LandingPage.js
@@ -1,34 +1,30 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./LandingPage.css";
-
-const Layout = () => {
+import Navbar from "../Navbar/Navbar";
+const Landing_Page = () => {
+<Navbar/>
   return (
     <section className="hero-section">
       <div>
         <div data-aos="fade-up" className="flex-hero">
-          <h1>
-            Your Health<br/>
-            <span className="text-gradient">
-              Our Responsibility
-            </span>
-          </h1>
-          <div className="blob-cont">
-            <div className="blue blob"></div>
-          </div>
-          <div className="blob-cont">
-            <div className="blue1 blob"></div>
-          </div>
-          <h4>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque at quae ducimus. Suscipit omnis quibusdam non cum rem voluptatem!
-          </h4>
-          <Link to="#services">
-            <button className="button">Get Started</button>
-          </Link>
+            
+            <h1>
+              Your Health<br/>
+
+              <span className="text-gradient">
+                
+                Our Responsibility
+              </span>
+            </h1>
+            <h4>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eaque at quae ducimus. Suscipit omnis quibusdam non cum rem voluptatem!
+            </h4>
+              <button class="button">Get Started</button>
         </div>
+
       </div>
     </section>
   );
 };
 
-export default Layout;
+export default Landing_Page;
