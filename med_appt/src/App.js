e874a97d6256283c02a0915f23a86e1f8e398ba9
Rohit Sayeeganesh <rsbby@mst.edu>
2024-03-28 13:29:38 -0500

    Added Notification

--- med_appt/src/App.js
+++ med_appt/src/App.js
@@ -4,10 +4,12 @@ import Navbar from './Components/Navbar/Navbar';
 import Landing_Page from './Components/Landing_Page/LandingPage';
 import Sign_Up from './Components/Sign_Up/Sign_Up';
 import Login from './Components/Login/Login';
-import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
-import FindDoctorSearchIC from './Components/InstantConsultation/FindDoctorSearchIC/FindDoctorSearchIC';
-import FindDoctorSearch from './Components/FindDoctorSearch/FindDoctorSearch';
+import InstantConsultation from './Components/InstantConsultationBooking/InstantConsultation';
+import FindDoctorSearchIC from './Components/FindDoctorSearchIC/FindDoctorSearchIC';
+import FindDoctorSearch from './Components/InstantConsultationBooking/FindDoctorSearchIC/FindDoctorSearchIC';
 import BookingConsultation from './Components/BookingConsultation';
+import Notification from './Components/Notification/Notification';
+
 function App() {
   return (
     <div className="App">
@@ -15,19 +17,19 @@ function App() {
          <Navbar/>
          <switch>
             <Routes>
-              <Route exact path="/" Component={Landing_Page} />
-              <Route path="/signup" Component={Sign_Up} />
-              <Route path="/login" Component={Login} />
-              <Route path="/instant-consultation" Component={ InstantConsultation } />
-              <Route path="/search/doctors" Component={FindDoctorSearch} />
-              <BookingConsultation/>
+            <Route exact path="/" Component={Landing_Page} />
+            <Route path="/singup" Component={Sign_Up} />
+            <Route path="/login" Component={Login} />
+            <Route path="/instant-consultation" Component={ InstantConsultation } />
+            <Route path="/search/doctors" Component={FindDoctorSearch} />
+            <Route path="/notification" Component={Notification} />
+            <BookingConsultation/>
+            
             </Routes>
          </switch>
         </BrowserRouter>
     </div>
   );
-
-}  
-
+}
 
 export default App;
